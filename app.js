7e450a9a064b6b70bf85e62582e094f2c2756fc4
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-09-18 17:17:16 -0400

    Book constructor created, added budget responsive design

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+// books to be stored in array
 const myLibrary = [];
 
-function Book() {
-
+// function for deleting book to be stored on prototype,
+// not within the constructor!
+function Book(title, author, pageNumber, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.readStatus = readStatus
 }
 
+Book.prototype.changeReadStatus = function () {
+    if (this.readStatus === true) {
+        // changing the books read status from "true" to "false"
+        return false
+    } else {
+        return true
+    }
+}
+
+
 function addBooktoLibrary() {
-    
+
 }
