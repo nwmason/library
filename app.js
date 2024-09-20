d7bcfe85ee019589171318100f5262bf577ad136
nwmason <[200~174368433+nwmason@users.noreply.github.com>
2024-09-20 16:12:07 -0400

    final (hopefully) adjustments to responsiveness added funcationallity for javascript array outputting to book container div

--- app.js
+++ app.js
@@ -29,10 +29,52 @@ function addBooktoLibrary() {
 
 myLibrary.push(theLordofTheRings)
 myLibrary.push(Lovecraft)
-
+console.log(myLibrary)
 // to do:
 // iterate over myLibrary, publishing book info
 // on each individual Book div in class book-container
-myLibrary.forEach(function (arrayItem) {
-    console.log(arrayItem.title)
+const libraryGrid = document.querySelector(".container")
+
+myLibrary.forEach(function(bookObject) {
+    const bookContainer = document.createElement("div");
+
+    bookContainer.setAttribute("class", "book-container");
+    libraryGrid.appendChild(bookContainer);
+
+    const TitleLabel = document.createElement("div");
+    const AuthorLabel = document.createElement("div");
+    const PagesLabel = document.createElement("div");
+    const ReadStatusLabel = document.createElement("div");
+
+    TitleLabel.setAttribute("class", "label");
+    TitleLabel.textContent = "Title:";
+    AuthorLabel.setAttribute("class", "label");
+    AuthorLabel.textContent = "Author:";
+    PagesLabel.setAttribute("class", "label");
+    PagesLabel.textContent = "Pages:";
+    ReadStatusLabel.setAttribute("class", "label");
+    ReadStatusLabel.textContent = "Have read?";
+
+    const bookTitle = document.createElement("div");
+    const bookAuthor = document.createElement("div");
+    const bookPages = document.createElement("div");
+    const bookReadStatusGrid = document.createElement("form");
+
+    bookTitle.setAttribute("class", "book-title");
+    bookTitle.textContent = bookObject.title;
+    bookAuthor.setAttribute("class", "book-author");
+    bookAuthor.textContent = bookObject.author;
+    bookPages.setAttribute("class", "book-pages");
+    bookPages.textContent = bookObject.pageNumber;
+    bookReadStatusGrid.setAttribute("id", "read-status");
+
+    bookContainer.appendChild(TitleLabel);
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(AuthorLabel);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(PagesLabel);
+    bookContainer.appendChild(bookPages);
+    bookContainer.appendChild(ReadStatusLabel);
+    bookContainer.appendChild(bookReadStatusGrid);
 });
+
